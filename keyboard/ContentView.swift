cb5332033a24d9e4cfb7799ce2caf88064cc0d9e
Woojin Lee <nijoow1127@gmail.com>
2026-04-08 15:53:38 +0900

    Initial Commit

---
+++ keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keyboard
+//
+//  Created by 이우진 on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keyboard/keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyboardApp.swift
+//  keyboard
+//
+//  Created by 이우진 on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
